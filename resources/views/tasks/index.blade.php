2a6f704b274ba32d9660b1b91acf3b725aef4e56
mimi.mi <zwmimi1212@gmail.com>
2020-11-05 11:36:18 +0000

    index_link_fixed

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -26,13 +26,5 @@
     @endif
     {{-- タスク登録ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'btn btn-primary']) !!}
-
-    @foreach ($tasks as $task)
-        <tr>
-            {{-- タスク詳細ページへのリンク --}}
-                <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                <td>{{ $task->content }}</td>
-        </tr>
-    @endforeach    
     
 @endsection
